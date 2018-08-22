c9d841578254bea5a9e773cffd7d3278864402df
tiagovalenca <36921610+tiagovalenca@users.noreply.github.com>
2018-08-22 10:37:47 -0300

    Diminuir tempo de execução

--- main.c
+++ main.c
@@ -7,11 +7,24 @@ int main(void) {
   scanf("%lu", &t);
   for(unsigned long int i = 0; i < t; i++){
     scanf("%lu %lu %lu %lu %lu %lu %lu", &a, &b, &x, &n, &c, &d, &m);
-    for(unsigned long int j = 0; j <= n; j++){
-      z = x + j;
-      y = (a*z + b) % m;
-      if (y >= c && y <= d){
-        counter++;
+    y = (a*x+b) % m;
+    if(a==0 && b==0 && c==0){
+      counter = n + 1;
+    }else if(m==1 && m>=c && m<=d){
+     counter = n + 1;
+    }else if(y>=c && y<=d && m>d && m>y && n>d && m>n){
+      if(c == 0 && d!=1){
+        counter = d-c+1;
+      }else {
+        counter = d-c;
+      }
+    } else{
+      for(unsigned long int j = 0; j <= n; j++){
+        z = x + j;
+        y = (a*z + b) % m;
+        if(y >= c && y <= d){
+          counter++;
+        }
       }
     }
     printf("%lu\n", counter);
